a21e7f32fe85b7b579d246f5c20fa7aaef58a9e5
Ann <annie.wang50@yahoo.com>
2021-08-16 15:58:53 -0400

    background style & navbar

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,8 @@
+import home from '../../assets/home.jpg'
 import './Home.css'
 export default function Home() {
   return (
-    <div className="Home">
+    <div className="Home" style={{backgroundImage: `url(${home})`}}>
       <div className="section-title">Home</div>
     </div>
   )
