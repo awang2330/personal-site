b2472822c7b32b111c47fad280623a68b51ac086
Ann <annie.wang50@yahoo.com>
2021-08-16 22:42:16 -0400

    some style for experience section

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,7 +2,7 @@ import home from '../../assets/home.jpg'
 import './Home.css'
 export default function Home() {
   return (
-    <div className="Home" style={{backgroundImage: `url(${home})`}}>
+    <div id="Home" style={{backgroundImage: `url(${home})`}}>
       <div className="section-title">Home</div>
     </div>
   )
