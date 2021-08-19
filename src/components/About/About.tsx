7524fe443b758160c0e0304c12bd767a97053353
Ann <annie.wang50@yahoo.com>
2021-08-19 12:35:27 -0400

    flex align about section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,18 +3,19 @@ import './About.css';
 export default function About() {
   return (
     <div id="About">
-      <div className="section-title">About</div>
-      <div className="about-display">
-        <div className="about-img">
-          <img src="" alt=""></img>
-        </div>
-        <div>
-          <p>I am interested in design, as well as full stack web development.</p>
-          <p>
-            Some things I enjoy outside outside of coding include growing plants and 
-            listening to folk music. I’ve also been learning a new language - Japanese!
-          </p>
-          
+      <div>
+        <div className="section-title">About</div>
+        <div className="about-display">
+          <div className="about-img">
+            <img src="" alt=""></img>
+          </div>
+          <div className="about-pg">
+            <p>I am interested in design, as well as full stack web development.</p>
+            <p>
+              Some things I enjoy outside outside of coding include growing plants and 
+              listening to folk music. I’ve also been learning a new language - Japanese!
+            </p>
+          </div>
         </div>
       </div>
     </div>
